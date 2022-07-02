54d6f0db83bd418e3f81e83ee75577a7d924e64e
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-02 18:17:50 -0400

    Added the team module

---
+++ mlbdata/team.py
@@ -0,0 +1,10 @@
+# Team Module
+
+class Team:
+    def __init__(self, status, name, score, wins, losses, pct):
+        self.status = status
+        self.name = name
+        self.score = score
+        self.wins = wins
+        self.losses = losses
+        self.pct = pct
