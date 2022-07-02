ec25f5013b94db6b544ec47ca32ee8827bbde2f9
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-02 18:17:44 -0400

    Added the game module

---
+++ mlbdata/game.py
@@ -0,0 +1,8 @@
+# Game Module
+
+class Game:
+    def __init__(self, time, venue, homeTeam, awayTeam):
+        self.time = time
+        self.venue = venue
+        self.homeTeam = homeTeam
+        self.awayTeam = awayTeam
