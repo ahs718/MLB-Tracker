1ef2110fc47474a302bf0ecab90784d47b166a94
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-02 23:02:35 -0400

    Added about and contact routes

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ def index():
     games = fetchData()
     return render_template('index.html', games=games)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 app.debug = True
 app.run()
